d4349b9025febd76b54ef823dfd91beb4e607c89
ghaith <a89ghaith@gmail.com>
2021-11-30 13:34:57 +0200

    change some of code to jqueary

--- js/menu.js
+++ js/menu.js
@@ -1,26 +1,25 @@
-const list = document.querySelectorAll('.list');
 const dataViewer = document.querySelector('.data-viewer');
-list.forEach(li => {
-    li.onclick = () => {
-        dataViewer.innerHTML = '';
-        document.querySelector('.loading-animation').style.display = 'flex';
-        document.querySelectorAll('#path_yallow').forEach(e => e.setAttribute("transform", "translate(23 -401)"))
-        document.querySelectorAll('#path_red').forEach(e => e.setAttribute("transform", "translate(197 -401)"))
-        li.nextElementSibling.firstElementChild.setAttribute("transform", "translate(-150 -401)")
-        li.nextElementSibling.lastElementChild.setAttribute("transform", "translate(23 -401)")
-        showMenu(li.innerHTML.toLowerCase());
-    }
-})
 
-function showMenu(fileName) {
-    var xReq = new XMLHttpRequest();
-    xReq.open('GET', 'js/'+ fileName +'.json');
-    xReq.onload = () => {
-        document.querySelector('.loading-animation').style.display = 'none';
-        xData = JSON.parse(xReq.responseText);
-        dataViewer.innerHTML = '';
-        for (let i = 0; i < xData.length; i++) {
-            dataViewer.innerHTML += `
+$(document).ready(function () {
+    $('.list').click(function () {
+        $('.data-viewer').innerHTML = '';
+        $('.loading-animation').css("display", "flex");
+        $('.path_yallow').attr("transform", "translate(23 -401)");
+        $('.path_red').attr("transform", "translate(197 -401)");
+        $(this).next().find('#path_yallow').attr("transform", "translate(-150 -401)");
+        $(this).next().find('#path_red').attr("transform", "translate(23 -401)");
+        showMenu((this).innerHTML.toLowerCase());
+    });
+
+    function showMenu(fileName) {
+        var xReq = new XMLHttpRequest();
+        xReq.open('GET', 'js/' + fileName + '.json');
+        xReq.onload = () => {
+            $('.loading-animation').css('display', 'none');
+            xData = JSON.parse(xReq.responseText);
+            dataViewer.innerHTML = '';
+            for (let i = 0; i < xData.length; i++) {
+                dataViewer.innerHTML += `
             <div class="sandwich col-6 col-lg-4 text-center mt-3">
             <img class="img-fluid" src="${xData[i].img}" alt="">
             <span class="d-block mt-3">${xData[i].name}</span>
@@ -29,10 +28,13 @@ function showMenu(fileName) {
                 Details</button>
             </div>
             `
+            }
         }
+        xReq.send();
     }
-    xReq.send();
-}
+
+});
+
 
 function showMenuExternal() {
     const dataViewer = document.querySelector('.data-viewer');
